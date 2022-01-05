0d9851be9b064e248e02020d4309f5222d49fc69
mohanadefm <72825467+mohanadefm@users.noreply.github.com>
2022-01-05 14:44:58 +0300

    My first commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,11 @@ import initTilt from "./tilt";
 import initSr from "./sr";
 import { addResume } from "./utils";
 import resume from "../assets/resume.pdf";
+import mohanad from "../assets/mohanad-methkal.pdf";
 
 export default function initApp() {
   initSr();
   initTilt();
   // comment this if you don't want to attach your resume
-  addResume(resume);
+  addResume(mohanad);
 }
